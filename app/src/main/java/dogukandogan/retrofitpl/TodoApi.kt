f0c05c23f625904c4e49a472168c6c57f85f9c31
Coders Club <hi@codersclub.co>
2022-05-18 00:19:46 +0300

    Retrofit Project has done.

---
+++ app/src/main/java/dogukandogan/retrofitpl/TodoApi.kt
@@ -0,0 +1,10 @@
+package dogukandogan.retrofitpl
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface TodoApi {
+
+    @GET("/todos")
+    suspend fun getTodos(): Response<List<Todo>>
+}
